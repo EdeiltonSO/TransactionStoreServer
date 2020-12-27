027ca6e5fe9ab2cf28bba77bc269e8a2abe8eb52
Edeilton Oliveira <edeiltonso@gmail.com>
2020-12-26 22:40:49 -0300

    post transactions working

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -25,7 +25,7 @@ transactionRouter.post('/', (request, response) => {
     );
     const transaction = createTransaction.execute({ title, value, type });
 
-    return transaction;
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
